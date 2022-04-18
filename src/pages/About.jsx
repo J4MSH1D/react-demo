f16ef458b184bd09c650e1ed5130005b7578ac46
JamshidAbduqodirovVue <jamshidabdukadirov01@gmail.com>
2022-04-18 21:43:44 +0300

    small changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
-        <div className="min-h-screen">
-            <h1>This is About Page</h1>
+        <div className="min-h-screen flex items-center justify-center text-3xl">
+            This is About Page
         </div>
     )
 }
